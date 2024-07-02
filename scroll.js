ed4c755c44f3f4f8ce65ffc5d47305e40669b715
maruhsilva <marua.desenvolvedor@gmail.com>
2024-07-02 18:49:53 -0300

    final

---
+++ scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.imagem', {duration: 3500});
+sr.reveal('.fa-solid', {duration: 3500});
+sr.reveal('.fa-brands', {duration: 3500});
+sr.reveal('.fa-regular', {duration: 3500});
+sr.reveal('.descricao', {duration: 3500});
+sr.reveal('.servicos', {duration: 2500});
+sr.reveal('.precos', { rotate: { x: 0, y:5000, z:0}, duration: 2500});
+sr.reveal('.contato', {duration: 2000});
