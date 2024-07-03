b8508d7f22d6dcef421ff364d269c11f4c40481b
maruhsilva <marua.desenvolvedor@gmail.com>
2024-07-02 21:20:56 -0300

    final

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     let header = this.document.querySelector ('#nav')
-    header.classList.toggle('rolagem',window.scrollY > 800)
+    header.classList.toggle('rolagem',window.scrollY > 100)
 })
